acef6b04dbf24ac279734848702b0e42c8c13e50
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2023-06-25 01:54:56 -0300

    feat(api): added pagination in route movies

--- apps/api/src/app/controllers/movies/fetch.ts
+++ apps/api/src/app/controllers/movies/fetch.ts
@@ -6,7 +6,11 @@ export async function fetch(request: FastifyRequest, reply: FastifyReply) {
     server: { prisma },
   } = request;
 
-  const { movies } = await fetchMoviesService(prisma);
+  const { page: pageQuery } = request.query as { page: string };
+
+  const page = pageQuery ? Number(pageQuery) : 1;
+
+  const { movies } = await fetchMoviesService(prisma, page);
 
   reply.status(200).send({ movies });
 }
--- apps/api/src/app/services/fetch-movies.service.ts
+++ apps/api/src/app/services/fetch-movies.service.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-export async function fetchMoviesService(prisma: PrismaClient) {
-  const movies = await prisma.movie.findMany();
+export async function fetchMoviesService(prisma: PrismaClient, page = 1) {
+  const take = 20;
+  const movies = await prisma.movie.findMany({
+    take,
+    skip: (page - 1) * take,
+  });
   return {
     movies,
   };
